63ccbd369c4fbbf3c3c7ea20ca5f4b7da77d9b4a
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-08-30 10:25:38 +0800

    feat: create notification and events

--- app/Listeners/SendChirpCreatedNotifications.php
+++ app/Listeners/SendChirpCreatedNotifications.php
@@ -3,10 +3,12 @@
 namespace App\Listeners;
 
 use App\Events\ChirpCreated;
+use App\Models\User;
+use App\Notifications\NewChirp;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
-class SendChirpCreatedNotifications
+class SendChirpCreatedNotifications implements ShouldQueue
 {
     /**
      * Create the event listener.
@@ -21,6 +23,8 @@ class SendChirpCreatedNotifications
      */
     public function handle(ChirpCreated $event): void
     {
-        //
+        foreach (User::whereNot('id', $event->chirp->user_id)->cursor() as $user) {
+            $user->notify(new NewChirp($event->chirp));
+        }
     }
 }
